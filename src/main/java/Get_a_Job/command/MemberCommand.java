0fe62cc9a641525600b85e29ddc8cf1513745ccf
jeong3 <129027311+jeong3@users.noreply.github.com>
2025-04-26 16:04:45 +0900

    회원등록 및 리스트

---
+++ src/main/java/Get_a_Job/command/MemberCommand.java
@@ -0,0 +1,32 @@
+package Get_a_Job.command;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class MemberCommand {
+	String memberNum;
+	
+	String memberName;
+	
+	String memberId;
+	@Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+			message = "영문자와 숫자 그리고 특수문자가 포함된 8글자 이상")
+	String memberPw;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	Date memberRegist;
+	
+	String gender;
+	
+	String memberEmail;
+	
+	String memberPhone;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	Date memberBirth;
+}
